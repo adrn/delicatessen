59124ed3b6619aff6780d12432f77c7c2fe98930
Rodrigo Luger <rodluger@gmail.com>
2020-09-09 14:52:16 -0400

    better delay schedule

--- delicatessen/templates/custom.js
+++ delicatessen/templates/custom.js
@@ -10,8 +10,11 @@ $(document).ready(function() {
   // lists where all items are visible. This is a hack,
   // since I couldn't figure out how to set the "size"
   // property of the "select" element within Bokeh or CSS.
-  setTimeout(fixSelectors, 100);
-  setTimeout(fixSelectors, 300);
-  setTimeout(fixSelectors, 500);
-  setTimeout(fixSelectors, 1000);
+  // Note that we need to do this after a small amount of
+  // time to ensure this happens *after* things are loaded
+  // on the screen!
+  var delays = [0, 100, 300, 1000, 5000];
+  for (i = 0; i < delays.length; i++) {
+    setTimeout(fixSelectors, delays[i]);
+  }
 });
