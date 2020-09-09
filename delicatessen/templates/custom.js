f7643d408cc6bc18a5a54a0f5eda02b1b4e15cf8
Rodrigo Luger <rodluger@gmail.com>
2020-09-09 14:28:35 -0400

    functionality to change tools

--- delicatessen/templates/custom.js
+++ delicatessen/templates/custom.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
+function fixSelectors() {
+  items = document.getElementsByName('deli-selector');
+  for (i = 0; i < items.length; i++) {
+    items[i].size = '8';
+  }
+}
 
+$(document).ready(function() {
   // Once the page loads, turn the drop-down menus into
   // lists where all items are visible. This is a hack,
   // since I couldn't figure out how to set the "size"
   // property of the "select" element within Bokeh or CSS.
-  setTimeout(function() {
-    items = document.getElementsByName('deli-selector');
-    for (i = 0; i < items.length; i++) {
-      items[i].size = '8';
-    }
-  }, 300);
+  setTimeout(fixSelectors, 100);
+  setTimeout(fixSelectors, 300);
+  setTimeout(fixSelectors, 500);
+  setTimeout(fixSelectors, 1000);
 });
